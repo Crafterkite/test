5b1e68a0ee1043a49efe5db3a0705c77558cdb46
Crafterkite <crafterkite@gmail.com>
2026-04-23 14:06:37 -0500

    Updated page.tsx

--- app/(dashboard)/dashboard/docs/editor/[id]/page.tsx
+++ app/(dashboard)/dashboard/docs/editor/[id]/page.tsx
@@ -305,7 +305,7 @@ function VerticalRuler() {
   const marks = Array.from({ length: 12 }, (_, i) => i); // inches
 
   return (
-    <div className="sticky top-[84px] w-10 flex-shrink-0 border-r border-border bg-muted/20 relative">
+    <div className="sticky top-[84px] w-5 flex-shrink-0 border-r border-border bg-muted/20 relative">
       <div className="relative h-full">
         {marks.map((m, i) => (
           <div
